5e0df98f52362cb36c394a425a9c2f4430d9e8fe
Victoria Lisovenko <v@zilis.ru>
2020-03-11 14:43:27 +0700

    add button

---
+++ src/pages/auth/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Info extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+        <p><button>жмяк</button></p>
+      </div>
+    );
+  }
+}
+
+export default Info;
