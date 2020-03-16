a507a582228dc511e695056cc567dd273171e040
Victoria Lisovenko <v@zilis.ru>
2020-03-17 00:31:00 +0700

    form view

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -1,12 +1,21 @@
 import React from "react";
+import './index.scss';
 
 class Info extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello</h1>
-        <p><button>жмяк</button></p>
+      //<div className="body">
+      <div className="first">
+        <p>
+          <div className="exit">x</div>
+        </p>
+            <input type="buuton" className="vvod login" value="логин"/>
+            <input type="buuton" className="vvod parol" value="пароль"/>
+        <p>
+          <div className="send">жмяк</div>
+        </p>
       </div>
+    //</div>
     );
   }
 }
